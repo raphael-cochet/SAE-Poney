3c010ce0de528f29e2a7a84bafd27b2348f7da17
Cochet Raphael <o22301249@or-iut-i204-f07.iut45.univ-orleans.fr>
2025-01-09 12:22:23 +0100

    Création de compte + connxion

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap5
+from flask_login import LoginManager
 import os
 from dotenv import load_dotenv
 
@@ -22,6 +23,15 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = "21788944-5131-46d6-9b18-8eb01323b575"
 
 db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    from .models import Personne
+    return Personne.query.get(int(user_id))
 
 from .models import *
 
